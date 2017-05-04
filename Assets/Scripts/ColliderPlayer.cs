c394a8bca327f2ccbe5fd0b715921e5a9a980aab
Emiliandro Firmino <ecdmf.tjd@gmail.com>
2017-05-04 00:54:43 -0700

    Project so far

---
+++ Assets/Scripts/ColliderPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ColliderPlayer : MonoBehaviour {
+
+	public string prxScene;
+	// Use this for initialization
+	void OnCollisionEnter2D (Collision2D other) {
+		if (other.gameObject.tag == "Finish") {
+			ReloadScene ();
+		}
+		if (other.gameObject.tag == "Next") {
+			CarregarCena (prxScene);
+		}
+	}
+	
+	void ReloadScene () {
+		Scene valor = SceneManager.GetActiveScene ();
+		CarregarCena (valor.name);
+	}
+
+	void CarregarCena(string valor){
+		SceneManager.LoadScene (valor);
+	}
+}
---
+++ Assets/Scripts/ColorCodeManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorCodeManager : MonoBehaviour {
+
+	public Sprite red, white, blue;
+	public SpriteRenderer manager;
+
+	public Collider2D[] redInPhase;
+	public Collider2D[] blueInPhase;
+
+	// Use this for initialization
+	void Start () {
+		SpriteChanger (white);
+		ColliderManager (white);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			SpriteChanger (white);
+			ColliderManager (white);
+		}
+		if (Input.GetKeyDown (KeyCode.X)) {
+			SpriteChanger (red);
+			ColliderManager (red);
+		}
+		if (Input.GetKeyDown (KeyCode.C)) {
+			SpriteChanger (blue);
+			ColliderManager (blue);
+		}
+	}
+
+	void SpriteChanger(Sprite valor){
+		manager.sprite = valor;
+	}
+
+	void ColliderManager(Sprite valor){
+
+		if (valor == red) {
+			foreach (Collider2D tangibilidade in blueInPhase) {
+				tangibilidade.enabled = false;
+			}
+
+			foreach (Collider2D tangibilidade in redInPhase) {
+				tangibilidade.enabled = true;
+			}
+		}
+
+		if (valor == white) {
+			foreach (Collider2D tangibilidade in redInPhase) {
+				tangibilidade.enabled = false;
+			}
+
+			foreach (Collider2D tangibilidade in blueInPhase) {
+				tangibilidade.enabled = false;
+			}
+		}
+
+		if (valor == blue) {
+			foreach (Collider2D tangibilidade in blueInPhase) {
+				tangibilidade.enabled = true;
+			}
+
+			foreach (Collider2D tangibilidade in redInPhase) {
+				tangibilidade.enabled = false;
+			}
+		} 
+
+	}
+}
